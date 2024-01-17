6e6fa4d26b7606f76ed0d4a2d43e447246a53544
kunalmandloi735 <kunalmandloi735@gmail.com>
2024-01-17 14:18:52 +0530

    add this.js file

---
+++ Scopes/This.js
@@ -0,0 +1,18 @@
+// const user = {
+//     firstName: "Ram",
+//     LastName: "Raghu",
+//     fullName: function(){
+//         console.log("Fullname is ", this.firstName, this.LastName)
+//         //console.log(this);
+//     }
+    
+//    }
+//     //console.log(this);
+
+//    user.fullName();
+let username = "Krishna"
+function x(){
+    console.log(username);;
+}
+
+x();
--- Scopes/scope.js
+++ Scopes/scope.js
@@ -1,28 +1,42 @@
-let a = 20;
-const b = 20;
-var c = 30;
-var c = 40; 
- // var  give no error
- // let a =30;  error
- // const b = 20;  give error;
-const arr = [1, 3, 5, 5, 7];
+// let a = 20;
+// const b = 20;
+// var c = 30;
+// var c = 40;
+// var  give no error
+// let a =30;  error
+// const b = 20;  give error;
+// const arr = [1, 3, 5, 5, 7];
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 // {}  // block
- 
-{
-    let a = 10;
-    const b = 20;
-    var c = 300; 
-    
-console.log("inner",a);
-console.log("inner",b);
-console.log("inner",c);           // var does not have block scope
-}
 
+// {
+//     let a = 10;
+//     const b = 20;
+//     var c = 300;
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log("inner",a);
+// console.log("inner",b);
+// console.log("inner",c);           // var does not have block scope
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function x() {
+//   let a = 10;
+
+//   console.log("I am in the x function", a);
+//   function y() {
+//     let b = 20;
+//     console.log("I am in the function y", a);
+//   }
+
+//   y();
+//   console.log("I am below the y function ", a);
+// }
+// x();
+
